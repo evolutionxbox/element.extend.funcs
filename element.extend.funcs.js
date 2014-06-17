d4cebb112a4be613e46d17f21a8907b3f292cb1d
Jonathan Cousins] <j.cousins@ry.com>
2014-06-17 16:13:51 +0100

    Added toggle class function

--- element.extend.funcs.js
+++ element.extend.funcs.js
@@ -1,1 +1,19 @@
 // Funcations which extend the standard element object
+
+// Toggle class
+Element.prototype.toggleClass = function(className) {
+  if (this.classList) {
+    this.classList.toggle(className);
+  } else {
+    var classes = this.className.split(' ');
+    var existingIndex = classes.indexOf(className);
+
+    if (existingIndex >= 0) {
+      classes.splice(existingIndex, 1);
+    } else {
+      classes.push(className);
+    }
+
+    this.className = classes.join(' ');
+  }
+};
